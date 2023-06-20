495446564b4bceeb29186d155ceb62ed9f4ef2a3
AntonKobyshev <antonkobyshev@gmail.com>
2023-06-20 20:37:48 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,16 +14,16 @@ export const HomePage = () => {
         editing, and deleting contacts. You will be able to easily find the
         desired contacts using the search function by name. We value your
         privacy and ensure the security of your data. All contacts are stored
-        locally on your device, and we do not share them with third parties.
+        on our database, and we do not share them with third parties. We
+          hope that our Phonebook application will make managing your contacts
+          easy and convenient. If you have any questions or issues, feel free to
+          contact our support team.
       </Paragraph>
       {!isLoggedIn && (
         <Paragraph>
           To get started, please <AuthItem to={'/register'}>Register</AuthItem>{' '}
           or <AuthItem to={'/login'}>Log in</AuthItem> to your account. Once
-          authenticated, you can add, edit, and delete contacts as you wish. We
-          hope that our Phonebook application will make managing your contacts
-          easy and convenient. If you have any questions or issues, feel free to
-          contact our support team.
+          authenticated, you can add, edit, and delete contacts as you wish. 
         </Paragraph>
       )}
 
