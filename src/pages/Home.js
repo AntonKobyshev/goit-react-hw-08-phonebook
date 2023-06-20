52ecf4bb2ceb76891389ea09b638069c404e6ada
AntonKobyshev <antonkobyshev@gmail.com>
2023-06-20 20:38:40 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,17 +13,17 @@ export const HomePage = () => {
         application provides a simple and intuitive interface for adding,
         editing, and deleting contacts. You will be able to easily find the
         desired contacts using the search function by name. We value your
-        privacy and ensure the security of your data. All contacts are stored
-        on our database, and we do not share them with third parties. We
-          hope that our Phonebook application will make managing your contacts
-          easy and convenient. If you have any questions or issues, feel free to
-          contact our support team.
+        privacy and ensure the security of your data. All contacts are stored on
+        our database, and we do not share them with third parties. We hope that
+        our Phonebook application will make managing your contacts easy and
+        convenient. If you have any questions or issues, feel free to contact
+        our support team.
       </Paragraph>
       {!isLoggedIn && (
         <Paragraph>
           To get started, please <AuthItem to={'/register'}>Register</AuthItem>{' '}
           or <AuthItem to={'/login'}>Log in</AuthItem> to your account. Once
-          authenticated, you can add, edit, and delete contacts as you wish. 
+          authenticated, you can add, edit, and delete contacts as you wish.
         </Paragraph>
       )}
 
